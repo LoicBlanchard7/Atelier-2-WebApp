022a89de7c9575b937476519428299abf7c5d35e
LoicBlanchard7 <90914694+LoicBlanchard7@users.noreply.github.com>
2023-03-29 20:39:15 +0200

    Update navbar

--- reunionou/src/App.vue
+++ reunionou/src/App.vue
@@ -1,43 +1,33 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <!-- Marque -->
+      <ul class="navbar-nav me-auto">
+        <li class="navbar-brand">
+          <router-link class="nav-link" to="/">Reunionou</router-link>
+        </li>
+      </ul>
 
-      <li class="navbar-brand">
-        <router-link class="nav-link" to="/">Reunionou</router-link>
-      </li>
-
-      <!-- Bouton de navigation mobile -->
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <!-- Liens de navigation -->
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <!-- Lien Home -->
-
-
-          <!-- Lien HelloWorld -->
           <li class="nav-item">
             <router-link class="nav-link" to="/helloWorld">HelloWorld</router-link>
           </li>
 
-          <!-- Lien Créer événement -->
           <li class="nav-item">
             <router-link class="nav-link" to="/createEvent">Créer événement</router-link>
           </li>
         </ul>
 
-        <!-- Liens de connexion et inscription alignés à droite -->
         <ul class="navbar-nav">
-          <!-- Lien Connexion -->
           <li class="nav-item">
             <router-link class="nav-link" to="/signIn">Connexion</router-link>
           </li>
 
-          <!-- Lien Inscription -->
           <li class="nav-item">
             <router-link class="nav-link" to="/signUp">Inscription</router-link>
           </li>
@@ -47,8 +37,6 @@
   </nav>
   <router-view />
 </template>
-
-
 
 <script>
 export default {
